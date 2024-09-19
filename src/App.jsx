08c083cef1207ce1f1ede35aeb191c95622fbc97
alberto marcos <amarcosarranz@gmail.com>
2024-09-19 16:23:02 +0200

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
 import { useState } from "react";
 
 import "./App.css";
-import { Navbar } from "./components/Navbar/Navbar";
+/* import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import recipes from "./assets/recipes.json"
-import { Recetas } from "./components/Recetas/Recetas";
-
+import { Recetas } from "./components/Recetas/Recetas"; */
 
 function App() {
-  const [data, setData] = useState(recipes)
-
-  
-
-  return (
-    <>
-      <div className="containerApp">
-
-        <Navbar />
-      <div className="side-recetas">
-        <div className="recetasGaleria">
-        {data.map((receta, index) => {
-      return(
-        <div key={index} >
-          <Recetas receta={receta} setData={setData} index={index} data={data}/>
-
-        </div>
-        )
-          })}
-
-        
-        </div>
-      
-      
-  
-        <Sidebar />
-      </div>
-    
-        <Footer />
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar } from "../components/Navbar/Navbar";
+import recipes from "../assets/recipes.json";
+import { Recetas } from "../components/Recetas/Recetas";
+import { Sidebar } from "../components/Sidebar/Sidebar";
+import { Footer } from "../components/Footer/Footer";
+import "./styleHome.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [data, setData] = useState(recipes);
+
+  return (
+    <div className="containerHome">
+      <Navbar />
+      <div className="side-recetas">
+        <Sidebar />
+        <div className="recetasGaleria">
+          {data.map((receta, index) => {
+            return (
+              <div key={index}>
+                <Link to={`/itemDetails/${receta.id}`}>
+                  <Recetas
+                    receta={receta}
+                    setData={setData}
+                    index={index}
+                    data={data}
+                  />
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
