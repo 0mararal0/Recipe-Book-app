e4e610abb7598b4263160045d979081494e294f1
Sofia Morales <sofimoralesroa92@gmail.com>
2024-09-20 00:17:26 +0200

    item details

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import recipes from "../assets/recipes.json";
 import { useParams } from "react-router-dom";
+import "./ItemDetails.css";
 
 export const ItemDetails = () => {
-  //const parametrosDinamicos = useParams();
-  const parametrosDinamicos = { id: "9e7f0d61-aa6b-418a-b56a-6418d19e6e1d" };
+  const parametrosDinamicos = useParams();
 
   const recetaSeleccionada = recipes.find((eachReceta) => {
     return eachReceta.id === parametrosDinamicos.id;
@@ -12,41 +12,44 @@ export const ItemDetails = () => {
 
   return (
     <div>
-      <h3>Detalles de la Receta</h3>
-
+      <div className="fondo-receta"></div>
       {recetaSeleccionada === undefined ? (
         <h2>No existe la receta</h2>
       ) : (
-        <div className="contendor">
-          <img
-            className="rectasImagenes"
-            src={recetaSeleccionada.image}
-            alt="food"
-          />
-          <h3>¿Cómo tener una {recetaSeleccionada.name} perfecta?</h3>
-          <h3>{recetaSeleccionada.calories}</h3>
-          <h3>{recetaSeleccionada.servings}</h3>
+        <div className="contenedor">
+          <div>
+            <h1>{recetaSeleccionada.name} </h1>
+            <div className="contenedor-span">
+              <span><b>Calorías:</b>  {recetaSeleccionada.calories}</span>
+              <span><b>Porciones:</b> {recetaSeleccionada.servings}</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
 
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+            <h3>Ingredientes</h3>
+            <ul>
+              <li>Lorem ipsum </li>
+              <li>Lorem ipsum </li>
+              <li>Lorem ipsum </li>
+              <li>Lorem ipsum </li>
+              <li>Lorem ipsum </li>
+            </ul>
+          </div>
 
-          <h3>Ingredinetes</h3>
-          <ul>
-
-            <li>Lorem ipsum </li>
-            <li>Lorem ipsum </li>
-            <li>Lorem ipsum </li>
-            <li>Lorem ipsum </li>
-            <li>Lorem ipsum </li>
-
-          </ul>
+          <div>
+            <img
+              className="recetasImagenes"
+              src={recetaSeleccionada.image}
+              alt="food"
+            />
+          </div>
         </div>
       )}
     </div>
