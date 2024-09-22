2020ba9afcb2eaa63b178e0df5bd6fffe9554ebc
alberto marcos <amarcosarranz@gmail.com>
2024-09-22 23:38:07 +0200

    navbar y home

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { ItemDetails } from "./pages/ItemDetails";
 import { NotFound } from "./pages/NotFound";
+import { FavoriteRecipes } from "./pages/FavoriteRecipes";
 
 /* import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
@@ -18,7 +19,10 @@ function App() {
     <Routes>
       <Route path={"/"} element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path={"/itemDetails/:recipe"} element={<ItemDetails />} /> 
+      <Route path={"/itemDetails/:recipe"} element={<ItemDetails />} />
+      <Route path={"/favoritesRecipes"} element={<FavoriteRecipes />} />
+      <Route path={"/createRecipe"} element={<NotFound />} />{" "}
+      {/* falta añadir vista */}
       <Route path={"*"} element={<NotFound />} />
     </Routes>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,53 @@
 import React, { useState } from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-import recipes from "../assets/data.json";//nuevo archivo de datos, podemos cambiar recipes a datos o lista de datos
+import recipes from "../assets/data.json"; //nuevo archivo de datos, podemos cambiar recipes a datos o lista de datos
 import { Recetas } from "../components/Recetas/Recetas";
 import { Sidebar } from "../components/Sidebar/Sidebar";
 import { Footer } from "../components/Footer/Footer";
 import "./styleHome.css";
 import { Link } from "react-router-dom";
+import iconComida from "../assets/images/iconComida.png";
+
+let dataInitial = recipes.slice(0).splice(0, 7);
 
 export const Home = () => {
   const [data, setData] = useState(recipes);
+  const [showData, setShowData] = useState([]);
+  const [countCard, setCountCard] = useState(8);
+
+  const handleAdd = () => {
+    setCountCard(countCard + 8);
+  };
 
   return (
     <div className="containerHome">
       <Navbar />
+      <div className="containerTitle">
+        <h3>Display...</h3>
+        <h5>¿Qué vas a comer hoy?</h5>
+      </div>
+      <div className="buttonSelect">
+        <button className="buttonEntrante">
+          <p>Entrante</p>
+          <img src={iconComida} alt="icono comida" />
+        </button>
+        <button className="buttonPrincipal">
+          <p>Principal</p>
+          <img src={iconComida} alt="icono comida" />
+        </button>
+        <button className="buttonPostre">
+          <p>Postre</p>
+          <img src={iconComida} alt="icono comida" />
+        </button>
+        <button className="buttonTodos">
+          <p>Todos</p>
+          <img src={iconComida} alt="icono comida" />
+        </button>
+      </div>
       <div className="side-recetas">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <div className="recetasGaleria">
-          {data.map((receta, index) => {
+          {data.slice(0, countCard).map((receta, index) => {
             return (
               <div key={index}>
                 <Link to={`/itemDetails/${receta.id}`}>
@@ -24,13 +55,18 @@ export const Home = () => {
                     receta={receta}
                     setData={setData}
                     index={index}
-                    data={data}//nuevo archivo de datos
+                    data={data} //nuevo archivo de datos
                   />
                 </Link>
               </div>
             );
           })}
         </div>
+      </div>
+      <div className="buttonMostrarMas">
+        <button onClick={handleAdd}>
+          <p>Mostrar mas</p>
+        </button>
       </div>
 
       <Footer />
