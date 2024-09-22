d23d6a444be91d886e8a020a921956a3e91635d9
Emma <emmamartinezgarcia2806@gmail.com>
2024-09-22 20:25:45 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import recipes from "./assets/data.json"; //nuevo archivo de datos, podemos cambiar recipes a datos o lista de datos
 
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -6,6 +7,7 @@ import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { ItemDetails } from "./pages/ItemDetails";
 import { NotFound } from "./pages/NotFound";
+import { FormularioEditarReceta } from "./components/FomularioEditarReceta/FormularioEditarReceta";
 
 /* import { Navbar } from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
@@ -14,11 +16,20 @@ import recipes from "./assets/recipes.json"
 import { Recetas } from "./components/Recetas/Recetas"; */
 
 function App() {
+  const [data, setData] = useState(recipes); // use useState acá para compartir por props y puse los atributos
+
   return (
     <Routes>
-      <Route path={"/"} element={<Home />} />
+      <Route path={"/"} element={<Home data={data} setData={setData} />} />
       <Route path="/about" element={<About />} />
-      <Route path={"/itemDetails/:recipe"} element={<ItemDetails />} /> 
+      <Route
+        path={"/itemDetails/:recipe"}
+        element={<ItemDetails data={data} setData={setData} />}
+      />
+      <Route
+        path={"/formularioEditarReceta/:identificadorReceta"}
+        element={<FormularioEditarReceta data={data} setData={setData} />}
+      />
       <Route path={"*"} element={<NotFound />} />
     </Routes>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./styleNavbar.css";
 import logo from "../../assets/images/logo.png";
+import CrearReceta from "../../pages/CrearRecetas";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
@@ -8,6 +10,9 @@ export const Navbar = () => {
       <div className="containerNavbar">
         <img src={logo} alt="logo" />
         <h1>Recetas</h1>
+        <Link to="/crear-receta">
+        <button className="crear-receta-btn">Crear Receta</button>
+      </Link> 
       </div>
     </div>
   );
