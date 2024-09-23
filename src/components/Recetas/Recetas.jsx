e44d33eef2a4ff385db6b622c04e1f2147cb6320
alberto marcos <amarcosarranz@gmail.com>
2024-09-23 13:10:33 +0200

    filtros y maquetación home

--- src/components/Recetas/Recetas.jsx
+++ src/components/Recetas/Recetas.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./styleRecetas.css";
 import { Link } from "react-router-dom";
 
-export const Recetas = (props) => { {/* lleve el estado a app */}
-  const handleDelete = () => {
+export const Recetas = (props) => {
+  {
+    /* lleve el estado a app */
+  }
+
+  /*  const handleDelete = () => {
     let prov;
     prov = props.data.slice(0);
     prov.splice(props.index, 1);
     props.setData(prov);
-  };
+  }; */
   return (
     <div className="comida">
       {/*  <Link to={`/recetas/${props.receta.id}`}>
@@ -16,12 +20,14 @@ export const Recetas = (props) => { {/* lleve el estado a app */}
         <img src={props.receta.image} alt="" />{" "}
       </Link> */}
       <img src={props.receta.imagen} alt="" /> {/* cambie image por imagen */}
-
       <div>
-        <h2>{props.receta.titulo}</h2>   {/* cambie title por titulo*/}
-        <p>{props.receta.calories > 250 ? "☹️" : "🙂"} </p>
+        <h2>{props.receta.titulo}</h2> {/* cambie title por titulo*/}
+        {/* <p>{props.receta.calories > 250 ? "☹️" : "🙂"} </p> */}
       </div>
-      <button onClick={handleDelete}>Borrar</button>
+      <div>
+        <p>⌛{props.receta.tiempo_de_preparacion}</p>
+        <p>⚙️{props.receta.dificultad}</p>
+      </div>
     </div>
   );
 };
