c649a5fcbabbbd2221261b460964956e2982a8f6
alberto marcos <amarcosarranz@gmail.com>
2024-09-23 22:32:33 +0200

    favoritos

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Recetas } from "../components/Recetas/Recetas";
 
-export const FavoriteRecipes = () => {
-  return <div>FavoriteRecipes</div>;
+export const FavoriteRecipes = ({ data }) => {
+  console.log(data);
+
+  return (
+    <>
+      <div>
+        {data
+          .filter((e) => e.favorito === 1)
+          .map((receta) => {
+            return (
+              <div key={receta.id}>
+                <Recetas receta={receta} data={data} />
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
 };
