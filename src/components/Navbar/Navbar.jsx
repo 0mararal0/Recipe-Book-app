df616a4da8bce8a0d7aa6807f69fb1918bf5ed3a
Sofia Morales <sofimoralesroa92@gmail.com>
2024-09-24 18:18:09 +0200

    Estilos al home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,29 +7,32 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <div className="containerNavbar">
-      <div className="contentNavbar">
-        <div className="containerLogoNavbar">
-          <img src={logo} alt="logo" />
-          <h1>El Rincón del Chef</h1>
-        </div>
-        <div className="containerLink">
-          <Link to={"/"} style={{ textDecoration: "none" }}>
-            <p>Home</p>
-          </Link>
-          <Link to={"/favoritesRecipes"} style={{ textDecoration: "none" }}>
-            <p>Recetas favoritas</p>
-          </Link>
-          <Link to={"/about"} style={{ textDecoration: "none" }}>
-            <p>Sobre Nosotros</p>
-          </Link>
-        </div>
-        <Link to={"/createRecipe"} style={{ textDecoration: "none" }}>
-          <div className="containerCreateRecipe">
-            <p>Crear Receta</p>
+    <div>
+      <div className="containerNavbar">
+        <div className="contentNavbar">
+          <div className="containerLogoNavbar">
+            <img src={logo} alt="logo" />
+            <h1>El Rincón del Chef</h1>
           </div>
-        </Link>
-    </div>
+          <div className="containerLink">
+            <Link to={"/"} style={{ textDecoration: "none" }}>
+              <p>Home</p>
+            </Link>
+            <Link to={"/favoritesRecipes"} style={{ textDecoration: "none" }}>
+              <p>Recetas favoritas</p>
+            </Link>
+            <Link to={"/about"} style={{ textDecoration: "none" }}>
+              <p>Sobre Nosotros</p>
+            </Link>
+          </div>
+          <Link to={"/createRecipe"} style={{ textDecoration: "none" }}>
+            <div className="containerCreateRecipe">
+              <p>Crear Receta</p>
+            </div>
+          </Link>
+      </div>
+      <div className="navbar-linea"></div>
+      </div>
     </div>
   );
 }
